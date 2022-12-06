d28d9def76e66dfda53a614fed0f2434744bd9d4
sam <samadhanbhusnar@gmail.com>
2022-12-06 19:43:00 +0530

    This is sam

---
+++ 5ath.py
@@ -0,0 +1,8 @@
+# . Write a Python program to perform following operations on string
+#  a) calculate length of string b)convert string in uppercase
+#  c) convert string in lower case 
+gets='Sangola'
+one=gets.upper()
+two=gets.lower()
+print(one)
+print(two)
