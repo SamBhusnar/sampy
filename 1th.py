419b567fb92e4e75e6c0db35a9aba5ace9ba8f3a
sam <samadhanbhusnar@gmail.com>
2022-12-06 18:22:48 +0530

    This is sam from india

--- 1th.py
+++ 1th.py
@@ -4,8 +4,6 @@ print("Press y or Y if your ans. is true otherwise n or N")
 inp=input("You are smile or not now\n");
 inp2=input("You are smile or not now\n")
 if(((inp=='y'or inp=='Y') and (inp2=='Y' or inp2=='y')) or ((inp=='n' or inp=='N') and (inp2=='n' or inp2=='N'))):
-
     print("True")
-
 else:
     print("False")
