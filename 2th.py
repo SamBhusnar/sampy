149394e2cb9ea2d5efc86bfe38d9ecebda772f1e
sam <samadhanbhusnar@gmail.com>
2022-12-06 18:36:10 +0530

    This is sam from pandharpur

--- 2th.py
+++ 2th.py
@@ -1,3 +1,6 @@
+#  Input a string, return a new string where "not " has been added to the front.
+#  but, if the string already begins with "not", return the string unchanged.
+
 get=input("Enter any string\n")
 save=get.startswith("not")
 if(save==True):
